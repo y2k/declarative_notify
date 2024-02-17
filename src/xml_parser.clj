e5b08023b3fedffe1ff651f8e5c7dd862bbdf6b2
y2k <itwisterlx@gmail.com>
2024-02-17 11:41:41 +0100

    refactoring

--- src/xml_parser.clj
+++ src/xml_parser.clj
@@ -1,101 +1,94 @@
-(defn parse_tg_feed [body]
+(defn parse_with_dsl [body dsl]
   (let [items []]
     (->
      (Promise/resolve (Response. body))
      (.then (fn [res]
-              (let [text_buffer (atom "")]
+              (let [text_buffer (atom "")
+                    rewriter (HTMLRewriter.)]
                 (->
-                 (HTMLRewriter.)
-                 (.on "div.tgme_widget_message_bubble"
-                      {:element (fn [element] (.push items [null null]))})
-                 (.on "div.tgme_widget_message_bubble *"
-                      {:element (fn [element]
-                                  (cond
-                                    (= null (.getAttribute element "class")) null
+                 (.reduce
+                  (.slice dsl 1)
+                  (fn [[rw index] [rl attr]]
+                    (if (Array/isArray attr)
+                      (.reduce
+                       (.slice attr 1)
+                       (fn [[rw index2] [rl2 attr2]]
+                         [(.on rw (str (get dsl 0) " " rl " " (get attr 0) " " rl2)
+                               (cond
 
-                                    (.startsWith (.getAttribute element "class") "tgme_widget_message_text")
-                                    (do
-                                      (reset text_buffer "")
-                                      (.onEndTag element (fn []
-                                                           (-> items (.at -1) (assoc! 0 (deref text_buffer)))
-                                                           (reset text_buffer ""))))
+                                 (= attr2 :inner_text)
+                                 {:element (fn [element]
+                                             (reset text_buffer "")
+                                             (.onEndTag
+                                              element
+                                              (fn []
+                                                (-> items (.at -1)  (.at index) (.at -1) (assoc! index2 (deref text_buffer)))
+                                                (reset text_buffer ""))))}
 
-                                    (.startsWith (.getAttribute element "class") "tgme_widget_message_date")
-                                    (-> items (.at -1) (assoc! 1 (.getAttribute element "href")))
+                                 :else
+                                 {:element (fn [element]
+                                             (-> items (.at -1) (.at index) (.at -1) (assoc! index2 (.getAttribute element attr2))))}))
+                          (+ 1 index2)])
+                       [(->
+                         rewriter
+                         (.on (str (get dsl 0) " " rl " " (get attr 0))
+                              {:element (fn [element]
+                                          (-> items
+                                              (.at -1)
+                                              (.at index)
+                                              (.push (.fill (Array. (- attr.length 1)) null))))}))
+                        0])
+                      null)
+                    [(.on rw (str (get dsl 0) " " rl)
+                          (cond
 
-                                    :else null))
-                       :text (fn [t]
-                               (reset text_buffer (str (deref text_buffer) t.text)))})
+                            (= attr :inner_text)
+                            {:element (fn [element]
+                                        (reset text_buffer "")
+                                        (.onEndTag element (fn []
+                                                             (-> items (.at -1) (assoc! index (deref text_buffer)))
+                                                             (reset text_buffer ""))))}
+
+                            (Array/isArray attr)
+                            {:element (fn [element]
+                                        (-> items (.at -1) (assoc! index [])))}
+
+                            :else
+                            {:element (fn [element]
+                                        (-> items (.at -1) (assoc! index (.getAttribute element attr))))}))
+                     (+ 1 index)])
+                  [(->
+                    rewriter
+
+                    (.on (get dsl 0)
+                         {:element (fn [element]
+                                     (.push items (.fill (Array. (- dsl.length 1)) null)))})
+
+                    (.on (str (get dsl 0) " *")
+                         {:text (fn [t]
+                                  (reset text_buffer (str (deref text_buffer) t.text)))}))
+
+                   0])
+                 first
                  (.transform res)))))
      (.then (fn [x] (.arrayBuffer x)))
      (.then (fn [] items)))))
 
-(comment
-
-  [:div {:class "tgme_widget_message_bubble"}
-   [:div {:class "tgme_widget_message_text"} :inner_text]
-   [:a {:class "tgme_widget_message_date"} :href]]
-
-  [:entry {}
-   [:link {} :href]
-   [:updated {} :inner_text]
-   [:id {} :inner_text]
-   [:title {} :inner_text]
-   [:content {}
-    [:a {} :href]
-    [:a {} :inner_text]]]
-
-  ;; HTML
-  [:div {:class "tgme_widget_message_bubble"}
-   {:text [:div {:class "tgme_widget_message_text"} :inner_text]
-    :url [:a {:class "tgme_widget_message_date"} :href]}]
-  ;; RSS
-  [:entry {}
-   {:link [:link {} :href]
-    :updated [:updated {} :inner_text]
-    :id [:id {} :inner_text]
-    :title [:title {} :inner_text]
-    :links [:content {} {:url [:a {} :href]
-                         :title [:a {} :inner_text]}]}]
-
-  comment)
+(defn parse_tg_feed [body]
+  (parse_with_dsl
+   body
+   [:div.tgme_widget_message_bubble
+    [:div.tgme_widget_message_text :inner_text]
+    [:a.tgme_widget_message_date :href]]))
 
 (defn parse_rss_feed [body limit]
-  (let [items []]
-    (->
-     (Promise/resolve (Response. body))
-     (.then (fn [res]
-              (let [text_buffer (atom "")]
-                (->
-                 (HTMLRewriter.)
-                 (.on "entry"
-                      {:element (fn [element]
-                                  (.push items [null null null null []]))})
-                 (.on "entry *"
-                      {:element (fn [element]
-                                  (defn- update_text [key]
-                                    (reset text_buffer "")
-                                    (.onEndTag element (fn []
-                                                         (assoc! (.at items -1) key (deref text_buffer))
-                                                         (reset text_buffer ""))))
-                                  (case element.tagName
-                                    :link (assoc! (.at items -1) 0 (.getAttribute element "href"))
-                                    :updated (update_text 1)
-                                    :id (update_text 2)
-                                    :title (update_text 3)
-                                    null))
-                       :text (fn [t]
-                               (reset text_buffer (str (deref text_buffer) t.text)))})
-                 (.on "entry content a"
-                      {:element
-                       (fn [element]
-                         (-> items (.at -1) (.at 4) (.push [null null]))
-                         (-> items (.at -1) (.at 4) (.at -1) (assoc! 0 (.getAttribute element "href")))
-                         (reset text_buffer "")
-                         (.onEndTag element
-                                    (fn []
-                                      (-> items (.at -1) (.at 4) (.at -1) (assoc! 1 (deref text_buffer)))
-                                      (reset text_buffer ""))))})
-                 (.transform res)))))
-     (.then (fn [x] (.arrayBuffer x)))
-     (.then (fn [] (.splice items 0 limit))))))
+  (parse_with_dsl
+   body
+   [:entry
+    [:link :href]
+    [:updated :inner_text]
+    [:id :inner_text]
+    [:title :inner_text]
+    [:content [:li
+               [:a :href]
+               [:a :inner_text]]]]))
